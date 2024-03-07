b16bcf3a1a803b293161519cb139160a452277a2
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-07 23:24:11 +1100

    finished the basics of the form

--- toDoList.js
+++ toDoList.js
@@ -3,10 +3,11 @@ const task = document.querySelector('#task')
 const form = document.querySelector('#taskForm')
 
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     const toDoList = document.querySelector('#toDoList')
-//     const newTask = document.createElement('li')
-//     // newTask.innerText =
-//     toDoList.append(newTask)
-// })
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const toDoList = document.querySelector('#toDoList')
+    const newTask = document.createElement('li')
+    newTask.innerText = task.value
+    toDoList.append(newTask)
+    task.value = ''
+})
