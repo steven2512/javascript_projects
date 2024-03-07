f24eaf00f6d82c9f08e4bbfe27e8b21e984f31bf
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-07 21:40:13 +1100

    some minor js changes

--- toDoList.js
+++ toDoList.js
@@ -1,4 +1,9 @@
-const toDoList = document.querySelector('#toDoList')
-toDoList.addEventListener('submit', function (e) {
-    e.preventDefault()
+const taskForm = document.querySelector('#taskForm')
+const task = document.querySelector('#task')
+const taskButton = document.querySelector('#taskButton')
+
+
+task.addEventListener('change', function (e) {
+    const newTask = document.createElement('li')
+    newTask.innerText = 
 })
