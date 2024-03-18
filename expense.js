1b396498fbd0d5b296ca97f6285e460275df64a4
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-18 14:59:57 +1100

    removed the transfer option

--- expense.js
+++ expense.js
@@ -9,13 +9,20 @@ const type = document.querySelector('#type')
 
 //gather user input for transaction and append to history section at the end
 function addTransaction() {
+    //creating new elements in the DOM
     let newTransaction = document.createElement('li');
     let newAmount = document.createElement('span')
     let newNote = document.createElement('span')
+    //Assigning class names to children elements
     newAmount.className = 'amount-rc'
     newNote.className = 'note-rc'
-    newAmount.innerText = amount.value
+    //Note for new transaction
     newNote.innerText = note.value
+
+
+    if (type.value) === 'Expense' {
+        newAmount.innerText = '+' + amount.value
+    }
     newTransaction.append(newAmount)
     history.append(newTransaction)
 }
